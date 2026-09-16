9e21f973a0de16ff444b2c5934e22c6dcad5a21a
lyhjoy90 <lyhjoy90@gmail.com>
2026-09-16 15:44:08 +0900

    0916 수요일 오후3시

--- todolist_react/src/components/TodoItemList.jsx
+++ todolist_react/src/components/TodoItemList.jsx
@@ -1,14 +1,31 @@
 import TodoItem from "./TodoItem";
+import { memo } from "react";
 
 const TodoItemList = ({ myTodos, myToggle, myRemove }) => {
+    // 배열의 값 하나하나를 화면 조각으로 바꾼다
+    const todoList = myTodos.map(
+        ({ id, text, checked }) => (
+            <TodoItem
+                id={id}
+                text={text}
+                checked={checked}
+                onToggle={myToggle}
+                onRemove={myRemove}
+                key={id}
+            />
+        )
+    );
+    
     return (
         <div>
-            <TodoItem text="안녕" />
-            <TodoItem text="리액트" />
-            <TodoItem text="반가워" />
-
+            {todoList}
         </div>
     );
 };
  
-export default TodoItemList;
+//export default TodoItemList;
+// myTodos 가 바뀔 때만 다시 그린다
+export default memo(
+    TodoItemList,
+    (prevProps, nextProps) => prevProps.myTodos === nextProps.myTodos
+);
